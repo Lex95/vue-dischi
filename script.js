6dfad0834ea19bee0c6ffb9a6d2abe98cd159b2a
Lex95 <andrea.lega1995@gmail.com>
2021-05-12 15:44:17 +0200

    risoluzione esercizio

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const app = new Vue({
     el: "#app",
     data: {
-        
+        albumList: []
+    },
+    methods: {
+        getAlbums() {
+            axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((resp) => {
+                this.albumList = resp.data.response
+            })
+        }
+    },
+    mounted() {
+        this.getAlbums();
     }
 })
