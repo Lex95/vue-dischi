f1cd609552118e031be02ca0934e9d21ebae0d95
Lex95 <andrea.lega1995@gmail.com>
2021-05-12 16:32:00 +0200

    aggiunta primo bonus

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const app = new Vue({
     el: "#app",
     data: {
-        albumList: []
+        albumList: [],
+        genresList: [],
+        selectedGenre: "all",
+    },
+    computed: {
+        selectedAlbums() {
+            if (this.selectedGenre == "all") {
+                return this.albumList
+            } else {
+                const result = [];
+                this.albumList.forEach(element => {
+                    if (element.genre == this.selectedGenre) {
+                        result.push(element)
+                    }
+                })
+                return result
+            }
+        }
     },
     methods: {
         getAlbums() {
             axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((resp) => {
                 this.albumList = resp.data.response
             })
+        },
+        getGenres() {
+            this.albumList.forEach(element => {
+                if (!this.genresList.includes(element.genre)) {
+                    this.genresList.push(element.genre)
+                }
+            });
         }
     },
     mounted() {
